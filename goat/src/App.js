537b508fa49bbec37ee95ee712fac525af4842d9
MissDine <enidnyatichi1212@gmail.com>
2022-07-22 10:35:52 +0300

    Feature edit

--- goat/src/App.js
+++ goat/src/App.js
@@ -1,10 +1,20 @@
 import "./index.css"
-// import {BrowserRouter as Router, Route, Switch} from "react-router-dom" ;
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom" ;
+import Navbar from './components/Navbar';
+import Home from "./Home"
 
 // npx json-server --watch data/db.json --port 8000-to use the json server
 function App() {
   return (
-    <div className="App"></div>
+    <Router><div className="App">
+    <Navbar/>
+    <Switch>
+    <Route exact path = "/">
+        <Home/>
+    </Route>
+    </Switch>
+    </div>
+    </Router>
   );
 }
 
---
+++ goat/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "../app.css"
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* <h1>Enid's Blog</h1> */}
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
