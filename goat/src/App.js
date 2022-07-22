480aa17fbff91e07f6bc44b261374f3a2a12909c
MissDine <enidnyatichi1212@gmail.com>
2022-07-22 10:01:56 +0300

    Feature edit

---
+++ goat/src/App.js
@@ -0,0 +1,13 @@
+import "./index.css"
+// import {BrowserRouter as Router, Route, Switch} from "react-router-dom" ;
+
+// npx json-server --watch data/db.json --port 8000-to use the json server
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
