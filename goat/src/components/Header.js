601637967bcb581b6680ea9bfe06cbb8733b83af
MissDine <enidnyatichi1212@gmail.com>
2022-07-22 17:49:37 +0300

    Feature edit

--- goat/src/components/Header.js
+++ goat/src/components/Header.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Header() {
   return (
     <div className='header'>
-        <img className = "head" src="../bg.jpg" alt="head" />
-        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..</p>
-        <button>Sign Up</button>
+        {/* <img className = "head" src="../bg.jpg" alt="head" /> */}
+        <p className='head'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.</p>
+        <button className='btn'>Sign Up</button>
     </div>
   )
 }
--- goat/src/components/Services.js
+++ goat/src/components/Services.js
@@ -4,7 +4,21 @@ function Services() {
   return (
     <div>
         <h1>Our Service</h1>
-        
+        <ul>
+            <li className='web'>
+                {/* <img src="../web-design.jpg" alt="web design" /> */}
+            </li>
+            <span>Web design</span>
+            <li>
+                <img src="" alt="" />
+            </li>
+            <li>
+                <img src="" alt="" />
+            </li>
+            <li>
+                <img src="" alt="" />
+            </li>
+        </ul>
     </div>
   )
 }
