2f76639de7664cf0bd9f9b26131d1516e3c123f1
MissDine <enidnyatichi1212@gmail.com>
2022-07-22 14:34:19 +0300

    Feature edit

--- goat/src/components/Navbar.js
+++ goat/src/components/Navbar.js
@@ -6,13 +6,13 @@ const Navbar = () => {
         <nav className="navbar">
             <>
             <div className="links">
-                <img src="/goat/Color logo - no background (3).png" alt="logo" />
+                <img className = "logo" src="/goat/Color logo - no background (3).png" alt="logo" />
                 <Link className = "link" to="/">Home</Link>
                 <Link className = "link" to="/about">About</Link>
                 <Link className = "link" to="/services">Services</Link>
                 <Link className = "link" to="/contact">Contact</Link>
                 <Link className = "link" to="/login">Login</Link>
-                <button className = "btn">Login</button>
+                {/* <button className = "btn">Login</button> */}
             </div>
             </>
             
