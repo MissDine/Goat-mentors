3fc0698a3760f3bff7b30a623c8396621e52ce05
MissDine <enidnyatichi1212@gmail.com>
2022-07-22 18:18:28 +0300

    Feature edit

--- goat/src/components/Services.js
+++ goat/src/components/Services.js
@@ -1,24 +1,21 @@
 import React from 'react'
+import web from "../web-design.jpg"
+import mark from "../marketing.jpg"
+import des from "../ui_ux.png"
+import gra from "../graph.jpg"
 
 function Services() {
   return (
     <div>
         <h1>Our Service</h1>
-        <ul>
-            <li className='web'>
-                {/* <img src="../web-design.jpg" alt="web design" /> */}
-            </li>
-            <span>Web design</span>
-            <li>
-                <img src="" alt="" />
-            </li>
-            <li>
-                <img src="" alt="" />
-            </li>
-            <li>
-                <img src="" alt="" />
-            </li>
-        </ul>
+        <img className='web' src={web} alt="web design"/>
+        <h3>Web Design</h3>
+        <img className='Dig' src={mark} alt="marketing" />
+        <h3>Digital marketing</h3>
+        <img className='Des' src={des} alt="ui-ux design" />
+        <h3>Ui-Ux Design</h3>
+        <img className='Gra' src={gra} alt="Graphical design" />
+        <h3>Graphic Design</h3>
     </div>
   )
 }
