bd9b3e8b9186527e97ef45c1c6bf37508d2ec43c
MissDine <enidnyatichi1212@gmail.com>
2022-07-25 10:30:24 +0300

    Feature edit

--- goat/src/components/Team.js
+++ goat/src/components/Team.js
@@ -11,7 +11,7 @@ function Team() {
             title = "C.E.O & Founder"
             about = "Lorem Ipsum"
             email = "alio@example.com"
-            photo = "https://images.pexels.com/photos/12576758/pexels-photo-12576758.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            photo = "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cmFuZG9tJTIwcGhvdG9zJTIwb2YlMjBwZW9wbGUlMjBtZW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
           />
           <TeamCard
             name = "Enid"
